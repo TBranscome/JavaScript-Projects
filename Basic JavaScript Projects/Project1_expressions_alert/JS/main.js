ae5fb802608d5a387e771ff2c3a03c179de74132
TBranscome <tbranscome11@gmail.com>
2021-02-02 15:42:54 -0500

    Create javascript project
    created javascript project for learning purposes

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert('Hello, Tyler!');//opens alert window
+document.write('Hello, Tyler!');//prints text on screen
+var A = " This is a string!" ;//defined the variable A to a string
+document.write(A)
+var name = 'Finley,';
+window.alert(name + A);
+document.write(" Finley said, \"GOOGOO GAHGAH!\"");//used quotations inside
+document.write(' \"Finley, I love'
++' you!\" -Emily');
+var B = " Baby" + " Boy";
+document.write(B);
+var Family = "The Branscome Children", Oldest="Grayson", Sister="Jaina", Baby="Finley";
+document.write(Oldest);//defined variables to strings
+var Family = Family.fontcolor("red");//changed font color for variable
+document.write(Family);
+var Oldest = Oldest.fontcolor("green");
+document.write(Family + Oldest);
+document.write(3 - 3);//math
