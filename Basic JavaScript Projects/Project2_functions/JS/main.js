b6890761602038dcb6db2195132b7d7bf9ddba61
TBranscome <tbranscome11@gmail.com>
2021-02-02 17:43:46 -0500

    create folder project2, add files
    Created new JS project, used JV to example functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {//defing and naming my first function
+    var str = "This text is red!";//giving the variable a value
+    var result = str.fontcolor("red");//making the font color red
+    document.getElementById("Red_Text").innerHTML = result;//the result of the element with Red_Text id
+}
+function myFunction() {//defing and naming the function for the p element
+    var sentence = "I am learning";//defining a variable and giving it a string value
+    sentence += " a lot from this book!";//using the += operator to concatenate
+    document.getElementById("Concatenate").innerHTML = sentence;//giving a value of the result into HTML element Concatenate id
+}
